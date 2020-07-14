ca7fdf72cc6185321a757f61813ad44a7fe0a438
zHeadCrusher <67909733+zHeadCrusher@users.noreply.github.com>
2020-07-14 09:59:25 -0300

    Create while.c

---
+++ while.c
@@ -0,0 +1,12 @@
+/* Usando a estrutura de repeticao do/while */
+#include <stdio.h>
+main()
+{
+int contador = 1;
+do
+{
+        printf("%d \n", contador);
+}
+while (++contador<= 10);
+return 0;
+}
