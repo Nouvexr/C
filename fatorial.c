a3347b54750205aff31a0c7ac20ae31deba7615a
zHeadCrusher <67909733+zHeadCrusher@users.noreply.github.com>
2020-07-27 11:24:46 -0300

    Create fatorial.c

---
+++ fatorial.c
@@ -0,0 +1,17 @@
+/* Funcao recursiva para fatoriais */
+#include <stdio.h>
+long fatorial(long) ;
+main() {
+int i ;
+for (i = 1; i <= 10; i++)
+printf("%2d! = %ld\n", i, fatorial(i));
+return 0;
+}
+/* Definição recursiva da funcao fatorial */
+long fatorial(long numero)
+{
+if (numero <= 1)
+return 1;
+else
+return (numero * fatorial(numero - 1));
+}
